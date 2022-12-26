fda6c4be8b654e8bfe6af5b398607908d06f9bea
panam-py <panampraisehebron@gmail.com>
2022-12-26 17:44:16 +0100

    [update]: changed User tablename and added new model for influencers

--- app/models.py
+++ app/models.py
@@ -1,14 +1,24 @@
 from sqlalchemy import Column, DateTime, String, Integer
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 
 from .database import Base
 
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
     id = Column(Integer, primary_key = True, index = True)
     email = Column(String, unique = True, nullable = False)
     password = Column(String, nullable = False)
     created_at = Column(DateTime(timezone=True), server_default = func.now())
     updated_at = Column(DateTime(timezone=True), onupdate = func.now())
 
+class Influencer(Base):
+    __tablename__ = "influencers"
+    id = Column(Integer, primary_key = True, index = True)
+    username = Column(String, unique = True, nullable = False)
+    follower_count = Column(Integer, nullable = False)
+    bio = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default = func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate = func.now())
+    user = relationship('User')
