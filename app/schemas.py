87779acdb0b9c49c3f2647b2954920b7e607d87a
panam-py <panampraisehebron@gmail.com>
2022-12-27 04:01:37 +0100

    [feat]: added schema for onboarding an influencer

--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,9 @@ from pydantic import BaseModel, EmailStr, constr
 class RegisterIn(BaseModel):
     email: EmailStr
     password: constr(min_length = 8)
+
+
+class OnBoardIn(BaseModel):
+    username: str
+    follower_count: int
+    bio: constr(max_length = 100)
