02edc6e65ae752e0fc8a021f05a57dec5855b975
panam-py <panampraisehebron@gmail.com>
2022-12-27 02:54:50 +0100

    [update]: changed response format in register endpoint and added login endpoint

--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, Response
 from fastapi.middleware.cors import CORSMiddleware
 
 from sqlalchemy.orm import Session
+
+from datetime import timedelta
 
 from .schemas import RegisterIn
 
 from .models import User
 
-from .utils import hash_password
+from .utils import hash_password, compare_password
 
 from .database import get_db
+
+from .config import settings
+
+from .auth import AuthJWT
+
+ACCESS_TOKEN_EXPIRES_IN = settings.ACCESS_TOKEN_EXPIRES_IN
+REFRESH_TOKEN_EXPIRES_IN = settings.REFRESH_TOKEN_EXPIRES_IN
 
 app = FastAPI()
 
@@ -23,11 +32,12 @@ app.add_middleware(
 )
 
 @app.post("/register")
-async def register(data: RegisterIn, db: Session = Depends(get_db)):
+async def register(data: RegisterIn, response: Response, db: Session = Depends(get_db)):
     """
     Endpoint to register a new user collecting email and
     password
     """
+    data.email = data.email.lower()
     user = db.query(User).filter(User.email == data.email).first()
     if user:
         raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, detail = f"A user already exists with the email {data.email}")
@@ -35,5 +45,25 @@ async def register(data: RegisterIn, db: Session = Depends(get_db)):
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    return new_user
+    del new_user.password
+    response.status_code = status.HTTP_201_CREATED
+    return {'status': 'success', 'data': new_user}
 
+
+@app.post("/login")
+async def login(data: RegisterIn, response: Response, db: Session = Depends(get_db), Auth: AuthJWT = Depends()):
+    """
+    """
+    user = db.query(User).filter(User.email == data.email.lower()).first()
+    if (not user) or (not compare_password(data.password, user.password)):
+        raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail = 'Incorrect email or password')
+
+    access_token = Auth.create_access_token(subject = str(user.id), expires_time = timedelta(minutes = ACCESS_TOKEN_EXPIRES_IN))
+    refresh_token = Auth.create_refresh_token(subject = str(user.id), expires_time = timedelta(minutes = REFRESH_TOKEN_EXPIRES_IN))
+
+    response.set_cookie('access_token', access_token, ACCESS_TOKEN_EXPIRES_IN * 60, ACCESS_TOKEN_EXPIRES_IN * 60, '/', None, False, True, 'lax')
+    response.set_cookie('refresh_token', refresh_token, REFRESH_TOKEN_EXPIRES_IN * 60, REFRESH_TOKEN_EXPIRES_IN * 60, '/', None, False, True, 'lax')
+
+    del user.password
+    response.status_code = status.HTTP_200_OK
+    return {'status': 'success', 'access_token': access_token, 'refresh_token': refresh_token, 'data': user}
